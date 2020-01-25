24f05bbc304e157c093dec1088291018130dcd85
JevGor <reawqk@mail.ru>
2020-01-25 10:28:49 +0100

    inner+ ok, inner - faulty, max 15 not set

--- script.js
+++ script.js
@@ -1,21 +1,92 @@
 const chips = document.getElementById('chips')
 const field = document.getElementById('field')
+var num = Number(chips.innerHTML) || 15
 
 chips.addEventListener('click', function () {
-    let i = Number(chips.innerHTML)
-    do {
-        i = i - 1
-        let div = document.createElement('div');
-        field.appendChild(div);
-        div.setAttribute("id", "chips-inner")
-        div.setAttribute("class", "chips-inner")
+    let div = document.createElement('div');
+    field.appendChild(div);
+    div.setAttribute("id", "chips-inner")
+    div.setAttribute("class", "chips-inner")
+    
+    function substractFromStack () {
+        num = num - 1
+        chips.innerHTML = num
     }
-    while (
-        i < 15
-    )
+    substractFromStack()
 })
 
 field.addEventListener('click', function (el) {
     let e = el.target
-    if (e.id == 'chips-inner') e.remove() && chips.innerHTML + 1;
+    if (e.id == 'chips-inner') e.remove();
+
+    function addToStack () {
+        num = num + 1
+        chips.innerHTLM = num
+    }
+    addToStack()
 })
+
+
+
+
+// chips.addEventListener ('click', function (e){
+//     let chipsStack = e.target
+//     chipsStack.appendChild(newChips)
+//     newChips.setAttribute("id", "chips-inner")
+//     newChips.setAttribute("class", "chips")
+//     newChips.setAttribute("draggable", "true")
+
+//     newChips.innerHTML = 1
+// })
+
+
+// document.addEventListener("drag", function( event ) {
+// }, false);
+
+// document.addEventListener("dragstart", function( event ) {
+//     // store a ref. on the dragged elem
+//     dragged = event.target;
+//     // make it half transparent
+//     event.target.style.opacity = .5;
+// }, false);
+
+// document.addEventListener("dragend", function( event ) {
+//     // reset the transparency
+//     event.target.style.opacity = "";
+// }, false);
+
+// /* events fired on the drop targets */
+// document.addEventListener("dragover", function( event ) {
+//     // prevent default to allow drop
+//     event.preventDefault();
+// }, false);
+
+// document.addEventListener("dragenter", function( event ) {
+//     // highlight potential drop target when the draggable element enters it
+//     if ( event.target.className == "field" ) {
+//         event.target.style.background = "";
+//     }
+
+// }, false);
+
+// document.addEventListener("dragleave", function( event ) {
+//     // reset background of potential drop target when the draggable element leaves it
+//     if ( event.target.className == "field" ) {
+//         event.target.style.background = "";
+//     }
+
+// }, false);
+
+// document.addEventListener("drop", function( event ) {
+//     // prevent default action (open as link for some elements)
+//     event.preventDefault();
+//     // move dragged elem to the selected drop target
+//     if ( event.target.className == "field" ) {
+//         event.target.style.background = "";
+//         dragged.parentNode.removeChild( dragged );
+//         event.target.appendChild( dragged );
+//     }
+
+// }, false);
+// chips.innerHTML = n;
+// console.log("added")
